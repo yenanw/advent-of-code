84dfc11e5ecf286402e3e68a42df1ba5f5378c2d
yenanw <yenanw@mail.com>
2022-12-01 10:32:45 +0100

    remove some pointless method calls

--- 2022/day01/day01.py
+++ 2022/day01/day01.py
@@ -5,16 +5,17 @@ def parse(fname):
     elves = []
     with open(fname) as f:
         calories = []
-        for line in f.readlines():
+        for line in f:
             try:
                 v = int(line.strip())
                 calories.append(v)
             except ValueError:
-                elves.append(calories.copy())
+                elves.append(calories)
                 calories = []
 
         if len(calories) != 0:
             elves.append(calories)
+
     return elves
 
 
@@ -22,8 +23,8 @@ def solve():
     elves = parse("input.txt")
     total_calories = list(map(sum, elves))
     total_calories.sort(reverse=True)
-    part1 = total_calories[0]
 
+    part1 = total_calories[0]
     part2 = sum(total_calories[0:3])
 
     return {"part 1": part1, "part 2": part2}
