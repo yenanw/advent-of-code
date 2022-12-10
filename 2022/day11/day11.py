b73a0f2ee05d4d7879263417081ce192394a03e0
yenanw <yenanw@mail.com>
2022-12-10 12:22:01 +0100

    generate templates for all future days cuz i can't

---
+++ 2022/day11/day11.py
@@ -0,0 +1,17 @@
+import pprint as pp
+
+def parse():
+    with open("input.txt") as f:
+        for line in f:
+            continue
+    return None
+
+def solve():
+    moves = parse()
+
+    part1 = None
+    part2 = None
+    return {"part 1": part1, "part 2": part2}
+
+
+pp.pprint(solve())
---
+++ 2022/day20/day20.py
@@ -0,0 +1,18 @@
+import pprint as pp
+
+def parse():
+    with open("input.txt") as f:
+        for line in f:
+            continue
+    return None
+
+def solve():
+    moves = parse()
+
+    part1 = None
+    part2 = None
+    return {"part 1": part1, "part 2": part2}
+
+    path = os.path.join(cws, d)
+
+pp.pprint(solve())
